8b5091d7051044a8c0bca2c58ec788b876698895
Quentin Lagache <qlagache@gmail.com>
2022-02-20 17:01:33 +0100

    backend route comics

--- routes/comics.js
+++ routes/comics.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const axios = require("axios");
 require("dotenv").config();
 
-router.get("/comics", (req, res) => {
+router.get("/comics/:id", (req, res) => {
   const getComics = async () => {
     try {
       const response = await axios.get(
-        "https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=" +
-          process.env.MARVEL_KEY
+        "https://lereacteur-marvel-api.herokuapp.com/comics/" + req.params.id + "?apiKey=" + process.env.MARVEL_KEY
       );
       console.log(response.data);
+      console.log("https://lereacteur-marvel-api.herokuapp.com/comics/" + req.params.id + "?apiKey=" + process.env.MARVEL_KEY);
       res.status(200).json(response.data);
     } catch (error) {
       console.error(error.message);
